34a6b46d05400c5dc1902681652eecf1bac35a60
lpr16 <53109280+lpr16@users.noreply.github.com>
2022-10-03 22:03:52 -0300

    Add files via upload

---
+++ bee1000.cpp
@@ -0,0 +1,23 @@
+/*******************************************
+1000
+
+Your first program in any programming language is usually "Hello World!". 
+In this first problem all you have to do is print this message on the screen.
+
+Input
+This problem has no input.
+
+Output
+You must print the message Hello World! and then the endline
+ *******************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cout << "Hello World!\n";
+    
+    return 0;
+}
